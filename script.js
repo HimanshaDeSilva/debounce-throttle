21507bbab24d605c450dad055d244d8a359989c1
Himansha Dilshangi <desilvahima@gmail.com>
2025-03-04 19:17:52 +0530

    commit throttling code

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ const defaultText = document.getElementById("default")
 const debounceText = document.getElementById("debounce")
 const throttleText = document.getElementById("throttle")
 
-const updateDebounceText = debounce((text)=>{
-    debounceText.textContent = text
-})
-
 input.addEventListener("input" ,e =>{
     defaultText.textContent = e.target.value;
-    updateDebounceText(e.target.value)
+    updateDebounceText(e.target.value);
+    updateThrottleText(e.target.value);
 })
 
 //Debounce
+const updateDebounceText = debounce((text)=>{
+    // debounceText.textContent = text
+    incrementCount(debounceText)// for mouse event
+})
+
 function debounce(callback,delay=1000){
     let timeout
     return(...args)=>{
@@ -22,3 +24,44 @@ function debounce(callback,delay=1000){
         },delay)
     }
 }
+
+//throttle
+const updateThrottleText = throttle((text)=>{
+    // throttleText.textContent = text
+    incrementCount(throttleText)// for mouse event
+})
+
+function throttle(callback,delay=1000){
+    let shouldWait = false;
+    let waitingArgs;
+    const timeoutFunc =()=>{
+        if(waitingArgs == null){
+            shouldWait = false
+        }else{
+            callback(...waitingArgs)
+            waitingArgs = null
+        }
+        setTimeout(timeoutFunc,delay)
+    }
+
+    return(...args)=>{
+        if(shouldWait) {
+            waitingArgs = args
+            return
+        }
+        callback(...args)
+        shouldWait = true
+
+        setTimeout(timeoutFunc,delay)
+    }
+}
+
+document.addEventListener("mousemove", e => {
+    incrementCount(defaultText)
+    updateDebounceText()
+    updateThrottleText()
+  })
+  
+  function incrementCount(element) {
+    element.textContent = (parseInt(element.innerText) || 0) + 1
+  }
